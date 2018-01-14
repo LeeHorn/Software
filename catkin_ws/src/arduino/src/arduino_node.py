80a34c7446952530e3be1f7b0d95fff6f80f553d
yungshansu <michael1994822@gmail.com>
2018-01-14 17:11:45 +0000

    final

--- catkin_ws/src/arduino/src/arduino_node.py
+++ catkin_ws/src/arduino/src/arduino_node.py
@@ -2,6 +2,7 @@
 import rospy
 import numpy as np
 from duckietown_msgs.msg import  BoolStamped
+from std_msgs.msg import Bool
 
 
 class arduinoROS(object):
@@ -10,8 +11,8 @@ class arduinoROS(object):
         self.button_b = BoolStamped()
 
         # =========== publisher ===========
-        self.pub_mid_grab = rospy.Publisher("/arduino/sub/grab", BoolStamped, queue_size=1)
-        self.pub_mid_drop = rospy.Publisher("/arduino/sub/drop", BoolStamped, queue_size=1)
+        self.pub_mid_grab = rospy.Publisher("/arduino/sub/grab", Bool, queue_size=1)
+        self.pub_mid_drop = rospy.Publisher("/arduino/sub/drop", Bool, queue_size=1)
 
         # =========== subscriber ===========
         self.sub_mid_A = rospy.Subscriber("~mid_A", BoolStamped, self.a_process, queue_size=1)
@@ -20,20 +21,25 @@ class arduinoROS(object):
 
    # =========== subscribe button a===========
     def a_process(self, msg):
-        state = BoolStamped()
-        state.header.stamp = msg.header.stamp
-        state.data = msg
+        state = Bool()
+        if(msg.data):
+            state.data = True
+        else:
+            state.data = False
+
         print "Button_A = ", state.data
-        self.pub_mid_grab.publish(state.data)
+        self.pub_mid_grab.publish(state)
         
 
     # =========== subscribe button b ===========
     def b_process(self, msg):
-        state = BoolStamped()
-        state.header.stamp = msg.header.stamp
-        state.data = msg
+        state = Bool()
+        if(msg.data):
+            state.data = True
+        else:
+            state.data = False
         print "Button_B = ", state.data
-        self.pub_mid_drop.publish(state.data)
+        self.pub_mid_drop.publish(state)
 
 if __name__ == "__main__":
     rospy.init_node("arduino_ros", anonymous = False)
